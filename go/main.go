8676dafb78c63ac8f7daac2b229100eb9d940fca
cc91x <Clagthorn91@protonmail.com>
2024-11-19 00:13:01 -0500

    Remove files, comments

--- go/main.go
+++ go/main.go
@@ -1,59 +1,8 @@
 package main
 
-/*
-
-go build -o ../bin/nba_main .
-
-../bin/nba_main --process=fetch_raw_odds --date=2024-11-16 --config=go_config.yaml
-
-we need to do fetch raw game for 10-27 - 11-17
-
-
-
-         lookup raw game data, save raw      --->    clean game data, save
-		/																   \
-date ---																	---> clean odds, save ---> combine game data with odds data into CSVs ---> analyze csv data
-		\ 															       /
- 		 lookup odds from source, save raw response ----------------------
-*/
-
-// How to run this? Do: go run .    from the terminal
-// go run main.go --date= --process=fetch_raw_games
-
-// TODOS:
-// Sanity check python script processing
-// Upload to git, hiding right amount of data
-// Update .gitignore
-
-// DONE FROM TODO:
-// Add log statements everywhere...
-// Consistency with naming return values, etc. -> should name most functions
-// Decide how to order funcs within a file
-// make the process names an enum,
-// add a comment depicting the flow of the data with the DB names
-// Convert every var arr [] to a pre allocated size
-// file organization, create a file for just constants, and another for helper funcs (right now they are one)
-// return if else with my ternary operator
-// move the db getter funcs to be one - but this is a bit of a stretch
-// add config file ... could use something like viper, but how would we keep it isolated in the constants file, and not main?
-//         -> I guess we can, using the init() function ... will have to try
-//			-> now need to implement it everywhere
-// Confirmed everything is passed by value when possible and reference when needed
-// More formal logging ... instead of fmt.println
-// Get the defer() func working for exiting - https://trstringer.com/golang-deferred-function-error-handling/
-// rename files
-// See if its worth making any of structs combined into one, like I do with the config struct... I know there are plenty of nested objects
-// 		-> decided against it, but I can reformat as I wish
-
 import (
 	"nba/helpers"
 )
-
-// "args": ["--process=clean_games", "--date=2024-03-18"] -> this seems to be working in launch.json
-// /bin/bash -c '/Users/ericwhitehead/desktop/clag/nba-project-post-mv/nba_processing_bin --process=clean_games --date=2024-10-22 --config=/Users/ericwhitehead/desktop/clag/nba-project-post-mv/go_config.yaml'
-
-// --process=clean_games --date=2024-10-22 --config=go_config.yaml
-// /bin/bash -c 'cd /Users/ericwhitehead/Desktop/clag/nba-project-post-mv/ && nba_processing_bin --process=clean_games --date=2024-10-22 --config=/Users/ericwhitehead/desktop/clag/nba-project-post-mv/go_config.yaml'
 
 func main() {
 	processName, date, logFile := helpers.Setup()
