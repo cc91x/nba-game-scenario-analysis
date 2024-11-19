b9c4cbd928d0ed2cb469ead361074df0aaa8a4b0
cc91x <Clagthorn91@protonmail.com>
2024-11-18 23:37:28 -0500

    Update nba_project_dag.py

--- airflow/dags/nba_project_dag.py
+++ airflow/dags/nba_project_dag.py
@@ -4,9 +4,9 @@ from datetime import datetime, timedelta
 
 # TODO: Populate these 
 # Path to python installation
-PYTHON_PATH = '/usr/local/bin/python3'
+PYTHON_PATH = ''
 # Absolute path to the root directory of the project
-PROJECT_HOME = '/Users/ericwhitehead/Desktop/clag/nba-project-post-mv'
+PROJECT_HOME = ''
 
 PYTHON_SCRIPTS_HOME = f'{PROJECT_HOME}/python'
 GO_PACKAGE_HOME = f'{PROJECT_HOME}/go'
